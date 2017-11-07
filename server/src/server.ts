d57059676b84ece12cf911c7cc877425838c82ef
Blake La Pierre <blakelapierre@gmail.com>
2017-11-07 15:08:46 -0500

    group results by url

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,7 +7,17 @@ const document: any = {};
 export default async () => {
   const browser = await puppeteer.launch();
 
-  await Promise.all(search('test', browser));
+  const results = await Promise.all(search('test', browser));
+
+  
+  const urls = results.reduce((urls, {name, results}, i) => {
+    return results.reduce((urls, {url}, i) => {
+      (urls[url] = urls[url] || []).push([name, i]);
+      return urls;
+    }, urls);
+  }, {});
+
+  console.log(urls);
 };
 
 function search(query, browser) {
@@ -21,9 +31,28 @@ function search(query, browser) {
     const results = await page.evaluate(evaluator);
   
     const resultsToPrint = 2;
-    console.log(`${name}\n`);
+    console.log(`${name} (${results.length} total results [showing first ${resultsToPrint}]):\n`);
     if (results) console.log(`  ${results.slice(0, resultsToPrint).map(result => `${result.titles[0]} (${result.url})\n    ${result.snippet}`).join('\n\n  ')}\n`);
   
     await page.close();
+
+    return {name, results};
   });
 }
+
+const groupBy = (list, selector, transform = a => a, groups = {}) => list.reduce((groups, item) => {
+  const value = selector(item);
+  (groups[value] = groups[value] || []).push(transform(item));
+  return groups;
+})
+
+// function groupBy(list, property) {
+//   return list.reduce((groups, item) => {
+//     const value = selector(item);
+//     (groups[value] = groups[value] || []).push(transform(item))
+//   });
+//   return list.reduce((groups, item) => {
+//     (groups[property] = groups[property] || []).push(item);
+//     return groups;
+//   }, {});
+// }
